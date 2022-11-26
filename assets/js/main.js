6ea89528fd6794234dd7a8e0e13a40d9ff63222c
unknown <ivnymetzker@gmail.com>
2022-11-25 22:45:48 -0300

    WIP - Adicionei o consumo da API para imagem, types, nome e numero do pokemon; Estilizei o botom para paginação

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,31 @@
 
 
-const url = 'https://pokeapi.co/api/v2/pokemon';
+function convertPokemonToLi(pokemon) {
+    return `
+    
+            <li class="pokemon ${pokemon.type}">
+
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+
+                    <img src="${pokemon.photo}"
+                        alt="${pokemon.name}">
+                </div>
 
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
-    .catch((error) => console.log(error))
+            </li>
+    
+    `
+}
+
+
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons = []) =>
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join(''))
